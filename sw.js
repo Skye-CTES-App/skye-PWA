dc124cb63addd362f54b4c7f030fdf197668b132
Skye-CTES-App <skye.ctes.app@gmail.com>
2026-04-02 17:23:26 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,54 +1,13 @@
-const CACHE_NAME = 'skye-v3-cache';
-const OFFLINE_URLS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'skye-v4-cache';
+const OFFLINE_URLS = ['./', './index.html', './manifest.json'];
 
-// Install: cache core files
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS)));
   self.skipWaiting();
 });
 
-// Activate: clean old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-// Fetch: network first for API calls, cache first for app files
 self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-
-  if (url.hostname === 'api.weather.gov') {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          const clone = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-          return response;
-        })
-        .catch(() => caches.match(event.request))
-    );
-    return;
-  }
-
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).then(response => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-        return response;
-      });
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
